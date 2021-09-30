8c5f05e56fcf044124634e9f53624444c65e9f87
SarahIsWeird <morningcrafter0603@gmail.com>
2021-09-30 11:02:57 +0200

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     application
     kotlin("jvm") version "1.5.31"
@@ -17,10 +13,26 @@ repositories {
     mavenCentral()
 }
 
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val klaxonVersion: String by project
+val exposedVersion: String by project
+val mysqlConnectorVersion: String by project
+
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    implementation("com.beust:klaxon:$klaxonVersion")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("mysql:mysql-connector-java:$mysqlConnectorVersion")
+
+    testImplementation("io.ktor:ktor-server-tests:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
 }
