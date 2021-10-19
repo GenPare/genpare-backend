75dbc30c163e672adf4869fc598cac905fd678bc
SarahIsWeird <morningcrafter0603@gmail.com>
2021-10-19 19:06:28 +0200

    GPB-6 Fix bigger than signed long error
    Kinda dirty fix, since we just take the sign away. The range is still big enough for like, forever though, and it doesn't cause any problems down the road.

--- src/main/kotlin/de/genpare/database/entities/Member.kt
+++ src/main/kotlin/de/genpare/database/entities/Member.kt
@@ -5,12 +5,13 @@ import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.math.absoluteValue
 import kotlin.random.Random
 
 class Member(id: EntityID<Long>) : LongEntity(id) {
     companion object : LongEntityClass<Member>(MemberTable) {
         fun newMember(init: Member.() -> Unit) =
-            new(Random.nextLong(), init)
+            new(Random.nextLong().absoluteValue, init)
 
         fun findByEmail(email: String) =
             transaction { Member.find { MemberTable.email eq email }.firstOrNull() }
