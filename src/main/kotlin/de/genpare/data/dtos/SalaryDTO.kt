5945e87f353ad8ea5e9dc8e83f8929e0fe240c4c
SarahIsWeird <morningcrafter0603@gmail.com>
2021-10-31 18:56:04 +0100

    GPB-14 Add salary fetching

---
+++ src/main/kotlin/de/genpare/data/dtos/SalaryDTO.kt
@@ -0,0 +1,13 @@
+package de.genpare.data.dtos
+
+import de.genpare.data.enums.Gender
+import de.genpare.data.enums.LevelOfEducation
+import de.genpare.data.enums.State
+
+data class SalaryDTO(
+    val salary: Int,
+    val gender: Gender,
+    val jobTitle: String,
+    val state: State,
+    val levelOfEducation: LevelOfEducation
+)
